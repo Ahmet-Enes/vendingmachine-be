7327188658e4098872f9bbf099b32bc5e4c43b13
ahmetenes <ahmetenes@sabanciuniv.edu>
2023-12-18 09:16:10 +0300

    Expired tokens are deleted at the end of the day

--- src/main/java/com/vending/machine/app/config/SchedulerConfig.java
+++ src/main/java/com/vending/machine/app/config/SchedulerConfig.java
@@ -1,5 +1,7 @@
 package com.vending.machine.app.config;
 
+import com.vending.machine.app.repository.TokenRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -8,10 +10,18 @@ import java.util.Date;
 
 @Configuration
 @EnableScheduling
-public class TempatureConfig {
+@RequiredArgsConstructor
+public class SchedulerConfig {
+    private final TokenRepository repository;
 
     @Scheduled(fixedDelayString = "${cool.machine.delay.string}")
     private void coolTheMachine() {
         System.out.println(new Date() + ": Dummy cooling operation");
+    }
+
+    @Scheduled(cron = "0 0 0 * * *")
+    private void deleteExpiredTokens() {
+        System.out.println("Deleting expired tokens...");
+        repository.findByValidUntilBefore(new Date()).collectList().flatMap(repository::deleteAll).subscribe();
     }
 }
--- src/main/java/com/vending/machine/app/repository/TokenRepository.java
+++ src/main/java/com/vending/machine/app/repository/TokenRepository.java
@@ -2,8 +2,12 @@ package com.vending.machine.app.repository;
 
 import com.vending.machine.app.modal.Token;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+
+import java.util.Date;
 
 public interface TokenRepository extends ReactiveMongoRepository<Token, String> {
     Mono<Token> findByToken(String token);
+    Flux<Token> findByValidUntilBefore(Date date);
 }
